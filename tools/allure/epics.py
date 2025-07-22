26306dc6030306581113fb30a4e4e1ad186ebb00
bogdanshcherban8 <bogdanshcherban8@users.noreply.github.com>
2025-07-22 15:38:25 +0000

    deploy: d77301db2d0450a2b90de2b96a6663a96130fa48

--- tools/allure/epics.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class AllureEpic(str, Enum):
-    AUTHORIZATION_SYSTEM = "Authorization System"
-    PRODUCT_DETAILS = "Product Details"
-    ERROR_HANDLING = "Error Handling"
--- tools/allure/features.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class AllureFeature(str, Enum):
-    AUTHORIZATION = "Authorization"
-    INVENTORY = "Inventory"
-    CART = "Cart"
-    CHECKOUT = "Checkout"
-    SIDEBAR = "SideBar"
